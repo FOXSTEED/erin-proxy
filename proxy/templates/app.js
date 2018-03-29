d4e413c8da51b3e329ca0d54e02f1e694f37f71b
EvaFro <frolli.erin@gmail.com>
2018-03-29 15:20:47 -0700

    create server side templates

---
+++ proxy/templates/app.js
@@ -0,0 +1,6 @@
+module.exports = (nearby, reviews, qa, overview) => `
+  <div id="Overview">${overview}</div>
+  <div id="QandA">${qa}</div>
+  <div id="Reviews">${reviews}</div>
+  <div id="Nearby">${nearby}</div>
+`
